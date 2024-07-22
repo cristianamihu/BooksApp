23f927a4ba4184d3fc0cc46b246c45d46e89ccd0
Cristiana-Maria Mihu <128689630+cristianamihu@users.noreply.github.com>
2024-07-22 19:04:26 +0300

    Add files via upload

---
+++ books-app-fe/src/app/reusable-something/reusable-something.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-reusable-something',
+  templateUrl: './reusable-something.component.html',
+  styleUrls: ['./reusable-something.component.css'],
+})
+export class ReusableSomethingComponent {
+  /*@Input() text: string;
+  secondText: string = '';
+
+  constructor() {
+    this.text = '';
+    console.log('constructor');
+  }
+
+  ngOnInit() {
+    this.secondText = 'my angular app';
+    console.log('ngOnInit');
+  }*/
+}
