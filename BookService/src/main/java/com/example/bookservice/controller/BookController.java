a907e43f1a014e62505ea321db20d2ac5d2855d6
Cristiana-Maria Mihu <128689630+cristianamihu@users.noreply.github.com>
2024-08-21 16:57:15 +0300

    Add files via upload

--- BookService/src/main/java/com/example/bookservice/controller/BookController.java
+++ BookService/src/main/java/com/example/bookservice/controller/BookController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
+@CrossOrigin(origins = "http://localhost:4200")
 public class BookController {
     private final BookRepository bookRepository;
 
